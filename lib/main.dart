e591936b0235bc8eb2bd63395b7698b309a32690
JP0030 <thekeen003@gmail.com>
2024-02-09 00:12:49 +0530

    game is redy

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -8,45 +9,55 @@ class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  _MyAppState createState() => _MyAppState();
+  MyAppState createState() => MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  // Game state variables
+class MyAppState extends State<MyApp> {
+
   List<String> board = List.filled(9, '');
   String currentPlayer = 'X';
   bool gameOver = false;
+  Duration delay = const Duration(milliseconds: 1500);
 
-  // Update board on cell tap
+
   void onCellTapped(int index) {
     if (!gameOver && board[index] == '') {
       setState(() {
         board[index] = currentPlayer;
         currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
         gameOver = checkWinner();
+        if (gameOver) {
+          Future.delayed(delay, () => resetGame());
+        }
       });
     }
   }
 
-  // Check for winner
+
   bool checkWinner() {
-    // Implement winning conditions logic
     for (int i = 0; i < 3; i++) {
       if (board[i] != '' && board[i] == board[i + 3] && board[i] == board[i + 6]) {
-        return true; // Row win
+        return true;
       }
       if (board[i * 3] != '' && board[i * 3] == board[i * 3 + 1] && board[i * 3] == board[i * 3 + 2]) {
-        return true; // Column win
+        return true;
       }
     }
     if (board[0] != '' && board[0] == board[4] && board[0] == board[8]) {
-      return true; // Diagonal win
+      return true;
     }
     if (board[2] != '' && board[2] == board[4] && board[2] == board[6]) {
-      return true; // Other diagonal win
+      return true;
     }
-    // Check for draw (all cells filled)
     return board.every((element) => element != '');
+  }
+
+  void resetGame() {
+    setState(() {
+      board = List.filled(9, '');
+      currentPlayer = 'X';
+      gameOver = false;
+    });
   }
 
   @override
@@ -84,14 +95,6 @@ class _MyAppState extends State<MyApp> {
               },
             ),
             if (gameOver) Text(gameOver ? '$currentPlayer wins!' : 'It\'s a draw!'),
-            if (!gameOver) ElevatedButton(
-              onPressed: () => setState(() {
-                board = List.filled(9, '');
-                currentPlayer = 'X';
-                gameOver = false;
-              }),
-              child: Text('Reset'),
-            ),
           ],
         ),
       ),
